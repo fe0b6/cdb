1f7f82e4a2383d26507b492f2f50e82f8d97a734
fe0b6 <spirit-dnk@yandex.ru>
2018-05-16 08:09:59 +0300

    add GetPlaceholderWithDiff

--- tools.go
+++ tools.go
@@ -24,6 +24,15 @@ func GetPlaceholder(l int) string {
 	return strings.Join(arr, ",")
 }
 
+// GetPlaceholderWithDiff - Формируем нужное кол-во placeholders со смещением
+func GetPlaceholderWithDiff(diff, l int) string {
+	arr := make([]string, l)
+	for i := diff + 1; i <= diff+l; i++ {
+		arr[i-1] = "$" + strconv.Itoa(i)
+	}
+	return strings.Join(arr, ",")
+}
+
 // GetMultiPlaceholder - Формируем нужное кол-во placeholders для множественной вставки
 func GetMultiPlaceholder(l, c int) string {
 	str := strings.TrimRight(strings.Repeat("?,", l), ",")
